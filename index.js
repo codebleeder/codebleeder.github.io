26fb6ab259a21de68db42402978faf400b0f5592
codebleeder <sharad.shivmath@gmail.com>
2017-09-12 21:12:15 -0400

    included menu item

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+
 $.when( $.ready ).then(function() {
-  console.log('document is ready');
+
 });
+
+function makeLinkActive(selectedItem){
+  var activeItem = $('.pure-menu-list').find('.pure-menu-active');
+  activeItem.removeClass('pure-menu-active');
+  selectedItem.parent().addClass('pure-menu-active');
+}
+
+ $('.pure-menu-link').click(function(){   
+   var selectedItem = $('#' + $(this).attr('id'));   
+   makeLinkActive(selectedItem);
+ });
